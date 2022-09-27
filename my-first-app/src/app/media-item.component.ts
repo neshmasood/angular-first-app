84c18c9111ea7fd3a46022fd70be7bd576d4d645
neshmasood <neshtam@gmail.com>
2022-09-27 15:11:41 -0500

    getting data to a component using @input function

--- my-first-app/src/app/media-item.component.ts
+++ my-first-app/src/app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-media-item',
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./media-item.component.css']
 })
 export class MediaItemComponent {
+    // @Input() is a media decorator, we can use this to get data to the component written after it in this case mediaItem. Property name is mediaItem which comes from app-media-item component.
+    @Input() mediaItem;
+
+
     onDelete(){
         console.log('deleted')
     }
