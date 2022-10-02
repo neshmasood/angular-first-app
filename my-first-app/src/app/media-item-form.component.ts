d762ed5f84a2ec3dbf8e91abc7cd2bd9400bf196
neshmasood <neshtam@gmail.com>
2022-10-02 17:05:04 -0500

    added form validators to make movie names in the form template

--- my-first-app/src/app/media-item-form.component.ts
+++ my-first-app/src/app/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -11,11 +11,14 @@ export class MediaItemFormComponent implements OnInit {
 
   constructor() {
     this.form = new FormGroup({
-      medium: new FormControl('Movies'),
-      name: new FormControl(''),
-      category: new FormControl(''),
-      year: new FormControl(''),
-    });
+        medium: new FormControl('Movies'),
+        name: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('[\\w\\-\\s\\/]+')
+        ])),
+        category: new FormControl(''),
+        year: new FormControl(''),
+      });
   }
   ngOnInit(){}
 
