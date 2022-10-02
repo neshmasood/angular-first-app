2dc8479b766d6d1b5520d4e9ae3473abb167646b
neshmasood <neshtam@gmail.com>
2022-10-02 02:43:05 -0500

    added media add form using angulat forms module and styled the form

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form.component';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,11 @@ import { CategoryListPipe } from './category-list.pipe';
     MediaItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
+    MediaItemFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
