8126d049841bca8152429ed1409085ade07a7303
neshmasood <neshtam@gmail.com>
2022-10-02 03:07:14 -0500

    added reactive forms module, oninit and form group and controls.

--- my-first-app/src/app/media-item-form.component.ts
+++ my-first-app/src/app/media-item-form.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
   styleUrls: ['./media-item-form.component.css']
 })
-export class MediaItemFormComponent {
-    onSubmit(mediaItem) {
-        console.log(mediaItem);
-    }
+export class MediaItemFormComponent implements OnInit {
+  form: FormGroup;
 
+  constructor() {
+    this.form = new FormGroup({
+      medium: new FormControl('Movies'),
+      name: new FormControl(''),
+      category: new FormControl(''),
+      year: new FormControl(''),
+    });
+  }
+  ngOnInit(){}
+
+  onSubmit(mediaItem) {
+    console.log(mediaItem);
+  }
 }
