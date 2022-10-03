4d931080687976d5b337fea7a6cdcc2fa396b4cf
neshmasood <neshtam@gmail.com>
2022-10-02 19:27:27 -0500

    added media-itemservice to a component

--- my-first-app/src/app/media-item-form.component.ts
+++ my-first-app/src/app/media-item-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { MediaItemService } from './media-item.service';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -9,7 +10,9 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class MediaItemFormComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder){
+  constructor(
+    private formBuilder: FormBuilder,
+    private mediaItemService: MediaItemService){
     this.form = this.formBuilder.group({
         medium: this.formBuilder.control('Movies'),
         name: this.formBuilder.control('', Validators.compose([
@@ -26,6 +29,6 @@ export class MediaItemFormComponent implements OnInit {
 
 
   onSubmit(mediaItem) {
-    console.log(mediaItem);
+    this.mediaItemService.add(mediaItem);
   }
 }
