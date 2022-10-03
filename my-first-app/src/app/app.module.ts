647293760d8a583598d55078729b5421044473a4
neshmasood <neshtam@gmail.com>
2022-10-02 19:55:26 -0500

    created and updated the app using providers.js to use lookup lists

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
 // import { MediaItemService } from './media-item-service';
+import { lookupListToken, lookupLists } from './providers';
 
-const lookupLists = {
-  mediums: ['Movies', 'Series']
-};
+// const lookupLists = {
+//   mediums: ['Movies', 'Series']
+// };
+// Move this to providers.js file in teh app
 
 @NgModule({
   declarations: [
@@ -27,7 +29,8 @@ const lookupLists = {
     ReactiveFormsModule
   ],
   providers: [
-    { provide: 'lookupListToken', useValue: lookupLists }
+    { provide: lookupListToken, useValue: lookupLists }
+    // { provide: 'lookupListToken', useValue: lookupLists } - We no longer need this after providers.js. We use the { provide: lookupListToken, useValue: lookupLists } instead
     // MediaItemService
   ],
   bootstrap: [AppComponent]
