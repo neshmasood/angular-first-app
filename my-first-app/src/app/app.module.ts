0c428897da618882616c1ea47d4df5a1f479f0a6
neshmasood <neshtam@gmail.com>
2022-10-02 19:03:01 -0500

    built a service with the movie data stored in the file

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
+import { MediaItemService } from './media-item-service';
 
 @NgModule({
   declarations: [
@@ -15,13 +16,15 @@ import { MediaItemFormComponent } from './media-item-form.component';
     MediaItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
-    MediaItemFormComponent
+    MediaItemFormComponent,
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    MediaItemService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
